5b54006ed9b71adba6c1154a2626304211684011
Joseph Hendele <josephhendele@gmail.com>
2015-01-07 15:23:55 -0600

    added block to migration

--- db/migrate/20150103212344_create_crimes.rb
+++ db/migrate/20150103212344_create_crimes.rb
@@ -1,16 +1,18 @@
 class CreateCrimes < ActiveRecord::Migration
   def change
-  	create_table :crimes do |t|
-  		t.string :date
-  		t.string :primary_type
-  		t.string :description
-  		t.string :location_description
-  		t.float :latitude
-  		t.float :longitude
-  		t.integer :community_area
-  		t.integer :district
+    create_table :crimes do |t|
+      t.string :date
+      t.string :primary_type
+      t.string :description
+      t.string :location_description
+      t.float :latitude
+      t.float :longitude
+      t.integer :community_area
+      t.integer :district
+      t.integer :temp
+      t.string :block
 
-  		t.timestamps
-  	end
+      t.timestamps
+    end
   end
 end
